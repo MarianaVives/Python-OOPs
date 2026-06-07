072fca6f3215a7b05231c96e1a1594cd658ef3aa
MarianaVives <marianavives123@gmail.com>
2026-06-07 19:34:58 +0100

    Update password manager

--- Day 29-30/password_manager.py
+++ Day 29-30/password_manager.py
@@ -10,6 +10,7 @@ img_full_path = os.path.join(current_dir, file_path)
 
 FONT_NAME = "Arial"
 FONT_NAME_2 = "Courier"
+
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 numbers = ["1","2","3","4","5","6","7","8","9","0"]
 symbols = ["#","$","%","&","*","@","!",".","+","-"]
@@ -18,6 +19,7 @@ num_letters = random.randint(8, 10)
 num_symbols = random.randint(2, 4)
 num_numbers = random.randint(2, 4)
 
+# --- Tkinter general configurations ---
 wnd = Tk()
 wnd.title("Password Manager")
 wnd.config(padx=50, pady=30)
@@ -27,6 +29,7 @@ logo_img = PhotoImage(file=img_full_path)
 canvas.create_image(125, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
+# --- Layout: Labels and Entries---
 website_text = Label(text="Website: ", font=(FONT_NAME, 10))
 website_text.grid(row=1, column=0)
 
@@ -47,7 +50,7 @@ email_user_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
 password_entry = Entry(width=30,font=(FONT_NAME_2, 10))
 password_entry.grid(row=3, column=1, sticky="W")
 
-#=== ADD BUTTON ===
+# --- Add a password ---
 def add_password():
     website = website_entry.get()
     email = email_user_entry.get()
@@ -58,7 +61,7 @@ def add_password():
         if confirm_entry(website, email, password):
             save_info(website, email, password)
 
-#=== GENERATE PWD BUTTON ===
+# --- Generate password randomly ---
 def generate_password():
     letters_password = [random.choice(letters) for _ in range(num_letters)]
     symbols_password = [random.choice(symbols) for _ in range(num_symbols)]
@@ -70,27 +73,28 @@ def generate_password():
     password_entry.insert(END, new_password)
     put_password_in_paperclip(new_password)
 
-#=== Save in TXT ===
+# --- Save data in TXT ---
 def save_info(website, email, password):
     with open("MyFile.txt", "a") as file:
         file.write(f"{website} | {email} | {password}\n")
     website_entry.delete(0, END)
     password_entry.delete(0, END)
 
-#--- Confirm data entry ---
+# --- Confirm data entry ---
 def confirm_entry(website, email, password):
     choice = messagebox.askokcancel(title=website, message=f"These are the details entered: \n Email: {email}, \n Password: {password} \n Is it ok to save?")
     return choice
 
-# ---Paper Clip using pip install paper ---
+# ---Use Paper Clip dependency functionality to add password to paperclip ---
 def put_password_in_paperclip(password):
     print("Copying password")
     pyperclip.copy(password)
 
+# --- Buttons ---
 generate_passw_btn = Button(text="Generate Password", font=(FONT_NAME, 10), command=generate_password)
 generate_passw_btn.grid(row=3, column=2)
 
 add_btn = Button(text="Add", font=(FONT_NAME, 10), command=add_password)
 add_btn.grid(row=4, column=1, columnspan=2, sticky="EW")
 
-wnd.mainloop(
+wnd.mainloop()
